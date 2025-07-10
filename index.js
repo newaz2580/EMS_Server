18d1efeefae2c1e058324540819208a0de0f3a2a
newaz2796 <newaz2796@gmail.com>
2025-07-10 23:36:08 +0600

    get user role api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port =process.env.PORT || 3000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // Employee_Management
 // SP22r4FKGAwNRutK
-app.use(express.json())
+app.use(express.json());
 app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -27,12 +27,46 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+        const usersCollection = client.db("employeeManagement").collection("users");
+    // const PurchaseServiceCollection = client
+    //   .db("serviceSharing")
+      // .collection("bookingService");
     // Send a ping to confirm a successful connection
+    app.post('/users',async(req,res)=>{
+      const userData=req.body
+      userData.created_at=new Date().toISOString()
+      userData.last_loggedIn=new Date().toISOString()
+      console.log(userData)
+      const query={email:userData.email}
+      const alreadyExisting=await usersCollection.findOne(query)
+      if(alreadyExisting){
+        const result=await usersCollection.updateOne(query,
+          { $set:{last_loggedIn:new Date().toISOString()}}
+        )
+        return res.send(result)
+      }
+      const result=await usersCollection.insertOne(userData)
+      res.send(result)
+    })
+
+    app.get('/user-role/:email',async(req,res)=>{
+     try{
+       const email=req.params.email
+      const user=await usersCollection.findOne({email})
+      if(!user){
+        return res.status(404).send({message:'user not found'})
+      }
+      res.send({role:user?.userRole})
+
+     }catch(error){
+      res.status(500).send({message:'internal server error',error:error.message})
+     }
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
